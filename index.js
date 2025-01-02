727d7354d108188db5e2aca94d95f3b86ddb342b
Bakar002 <abubakaramjad78600@gmail.com>
2025-01-02 14:31:31 +0500

    adding

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://job-frontend-belike.vercel.app',
+    origin:'https://job-frontend-tau.vercel.app',
     credentials:true
 }
 
